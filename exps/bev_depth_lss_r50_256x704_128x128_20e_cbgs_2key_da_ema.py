543ad5fcd458fc14a684a3260935b48ac95ed6b9
Richard Hendricks <liyinhao0413@qq.com>
2022-08-27 15:08:51 +0800

    Remove unnecessary code.

--- exps/bev_depth_lss_r50_256x704_128x128_20e_cbgs_2key_da_ema.py
+++ exps/bev_depth_lss_r50_256x704_128x128_20e_cbgs_2key_da_ema.py
@@ -25,109 +25,15 @@ from argparse import ArgumentParser, Namespace
 
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
-from torch.cuda.amp.autocast_mode import autocast
 
 from callbacks.ema import EMACallback
-from exps.bev_depth_lss_r50_256x704_128x128_24e_2key_ema import \
+from exps.bev_depth_lss_r50_256x704_128x128_20e_cbgs_2key_da import \
     BEVDepthLightningModel as BaseBEVDepthLightningModel
-from layers.backbones.lss_fpn import LSSFPN as BaseLSSFPN
-from layers.heads.bev_depth_head import BEVDepthHead
-from models.bev_depth import BEVDepth as BaseBEVDepth
-
-
-class DepthAggregation(nn.Module):
-    """
-    pixel cloud feature extraction
-    """
-    def __init__(self, in_channels, mid_channels, out_channels):
-        super(DepthAggregation, self).__init__()
-
-        self.reduce_conv = nn.Sequential(
-            nn.Conv2d(in_channels,
-                      mid_channels,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1,
-                      bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-        )
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(mid_channels,
-                      mid_channels,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1,
-                      bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels,
-                      mid_channels,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1,
-                      bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-        )
-
-        self.out_conv = nn.Sequential(
-            nn.Conv2d(mid_channels,
-                      out_channels,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1,
-                      bias=True),
-            # nn.BatchNorm3d(out_channels),
-            # nn.ReLU(inplace=True),
-        )
-
-    @autocast(False)
-    def forward(self, x):
-        x = self.reduce_conv(x)
-        x = self.conv(x) + x
-        x = self.out_conv(x)
-        return x
-
-
-class LSSFPN(BaseLSSFPN):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.depth_aggregation_net = self._configure_depth_aggregation_net()
-
-    def _configure_depth_aggregation_net(self):
-        """build pixel cloud feature extractor"""
-        return DepthAggregation(self.output_channels, self.output_channels,
-                                self.output_channels)
-
-    def _forward_voxel_net(self, img_feat_with_depth):
-        # BEVConv2D [n, c, d, h, w] -> [n, h, c, w, d]
-        img_feat_with_depth = img_feat_with_depth.permute(
-            0, 3, 1, 4, 2).contiguous()  # [n, c, d, h, w] -> [n, h, c, w, d]
-        n, h, c, w, d = img_feat_with_depth.shape
-        img_feat_with_depth = img_feat_with_depth.view(-1, c, w, d)
-        img_feat_with_depth = (
-            self.depth_aggregation_net(img_feat_with_depth).view(
-                n, h, c, w, d).permute(0, 2, 4, 1, 3).contiguous().float())
-        return img_feat_with_depth
-
-
-class BEVDepth(BaseBEVDepth):
-    def __init__(self, backbone_conf, head_conf, is_train_depth=True):
-        super(BaseBEVDepth, self).__init__()
-        self.backbone = LSSFPN(**backbone_conf)
-        self.head = BEVDepthHead(**head_conf)
-        self.is_train_depth = is_train_depth
 
 
 class BEVDepthLightningModel(BaseBEVDepthLightningModel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model = BEVDepth(self.backbone_conf,
-                              self.head_conf,
-                              is_train_depth=True)
         self.data_use_cbgs = True
 
     def configure_optimizers(self):
