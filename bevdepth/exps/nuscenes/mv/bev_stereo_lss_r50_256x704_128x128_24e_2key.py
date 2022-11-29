4b505e07d5e02704380382e39e26f021841b80e9
Richard Hendricks <liyinhao0413@qq.com>
2022-11-29 21:57:30 +0800

    Update docstring for bevstereo exp

--- bevdepth/exps/nuscenes/mv/bev_stereo_lss_r50_256x704_128x128_24e_2key.py
+++ bevdepth/exps/nuscenes/mv/bev_stereo_lss_r50_256x704_128x128_24e_2key.py
@@ -1,26 +1,25 @@
 # Copyright (c) Megvii Inc. All rights reserved.
 """
-mAP: 0.3304
-mATE: 0.7021
-mASE: 0.2795
-mAOE: 0.5346
-mAVE: 0.5530
-mAAE: 0.2274
-NDS: 0.4355
-Eval time: 171.8s
-
+mAP: 0.3456
+mATE: 0.6589
+mASE: 0.2774
+mAOE: 0.5500
+mAVE: 0.4980
+mAAE: 0.2278
+NDS: 0.4516
+Eval time: 158.2s
 Per-class results:
 Object Class    AP      ATE     ASE     AOE     AVE     AAE
-car     0.499   0.540   0.165   0.211   0.650   0.233
-truck   0.278   0.719   0.218   0.265   0.547   0.215
-bus     0.386   0.661   0.211   0.171   1.132   0.274
-trailer 0.168   1.034   0.235   0.548   0.408   0.168
-construction_vehicle    0.075   1.124   0.510   1.177   0.111   0.385
-pedestrian      0.284   0.757   0.298   0.966   0.578   0.301
-motorcycle      0.335   0.624   0.263   0.621   0.734   0.237
-bicycle 0.305   0.554   0.264   0.653   0.263   0.006
-traffic_cone    0.462   0.516   0.355   nan     nan     nan
-barrier 0.512   0.491   0.275   0.200   nan     nan
+car     0.510   0.525   0.165   0.188   0.510   0.226
+truck   0.288   0.698   0.220   0.205   0.443   0.227
+bus     0.378   0.622   0.210   0.135   0.896   0.289
+trailer 0.156   1.003   0.219   0.482   0.609   0.179
+construction_vehicle    0.094   0.929   0.502   1.209   0.108   0.365
+pedestrian      0.356   0.728   0.297   1.005   0.579   0.319
+motorcycle      0.361   0.571   0.258   0.734   0.631   0.211
+bicycle 0.318   0.533   0.269   0.793   0.208   0.007
+traffic_cone    0.488   0.501   0.355   nan     nan     nan
+barrier 0.506   0.478   0.277   0.200   nan     nan
 """
 from bevdepth.exps.nuscenes.base_cli import run_cli
 from bevdepth.exps.nuscenes.base_exp import \
